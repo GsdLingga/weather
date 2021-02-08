fc7b5f943aea1d175c09530f904e0704176bec19
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-02-09 03:15:38 +0800

    insert

--- air.php
+++ air.php
@@ -11,7 +11,7 @@
     $sql = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`latitude`, tb_air.`longitude`, tb_air.`interval`) VALUES ('$lat', '$lng', '$interval')");
     if ($sql) {
         $response = "sukses";
-        $data;
+        echo $data;
     } else {
         $response = "gagal";
     }
