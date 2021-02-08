276a23c4a55e7d8b110b79d0583c845800bf545e
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-02-09 03:18:31 +0800

    insert

--- air.php
+++ air.php
@@ -5,8 +5,10 @@
     
     $data = json_decode($request, true);
     // $id = mysqli_real_escape_string($dbconnect, $data['id']);
-    $lat = mysqli_real_escape_string($dbconnect, $data['lat']);
-    $lng = mysqli_real_escape_string($dbconnect, $data['lng']);
+    // $lat = mysqli_real_escape_string($dbconnect, $data['lat']);
+    // $lng = mysqli_real_escape_string($dbconnect, $data['lng']);
+    $lat = $_POST($dbconnect, $data['lat']);
+    $lng = $_POST($dbconnect, $data['lng']);
     $interval = date("Y-m-d h:i:s");
     $sql = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`latitude`, tb_air.`longitude`, tb_air.`interval`) VALUES ('$lat', '$lng', '$interval')");
     if ($sql) {
