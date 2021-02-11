4a9e7e0d13169ce412c93b62a75bde0b9f512616
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-02-12 04:48:57 +0800

    firebase

--- air.php
+++ air.php
@@ -8,13 +8,16 @@
     $temp = mysqli_real_escape_string($dbconnect, $data['temp']);
     $hum = mysqli_real_escape_string($dbconnect, $data['hum']);
     $interval = date("Y-m-d h:i:s");
-    $sql = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`interval`) VALUES ('$temp', '$hum', '$interval')");
-    if ($sql) {
-        $response = "sukses";
-        echo $data;
-    } else {
-        $response = "gagal";
+    if ($temp == null && $hum == null) {
+        echo "Input tidak boleh kosong";
+    }else{
+        $sql = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`interval`) VALUES ('$temp', '$hum', '$interval')");
+        if ($sql) {
+            $response = "sukses";
+            echo $data;
+        } else {
+            $response = "gagal";
+        }
+        echo $response;
     }
-    echo $response;
-    echo $suhu;
 ?>
