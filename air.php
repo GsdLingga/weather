e38d5061ec2b0114e8e41e47a5b1b870b4f1110f
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-02-12 03:37:54 +0800

    insert

--- air.php
+++ air.php
@@ -5,10 +5,10 @@
     
     $data = json_decode($request, true);
     // $id = mysqli_real_escape_string($dbconnect, $data['id']);
-    $lat = mysqli_real_escape_string($dbconnect, $data['lat']);
-    $lng = mysqli_real_escape_string($dbconnect, $data['lng']);
+    $suhu = mysqli_real_escape_string($dbconnect, $data['suhu']);
+    $kelembaban = mysqli_real_escape_string($dbconnect, $data['kelembaban']);
     $interval = date("Y-m-d h:i:s");
-    $sql = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`latitude`, tb_air.`longitude`, tb_air.`interval`) VALUES ('$lat', '$lng', '$interval')");
+    $sql = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`interval`) VALUES ('$suhu', '$kelembaban', '$interval')");
     if ($sql) {
         $response = "sukses";
         echo $data;
