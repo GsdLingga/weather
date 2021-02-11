da76fb3529f708d029c7aa8b987aeba7b69a0c0e
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-02-12 04:16:29 +0800

    firebase

--- air.php
+++ air.php
@@ -5,10 +5,10 @@
     
     $data = json_decode($request, true);
     // $id = mysqli_real_escape_string($dbconnect, $data['id']);
-    $suhu = mysqli_real_escape_string($dbconnect, $data['suhu']);
-    $kelembaban = mysqli_real_escape_string($dbconnect, $data['kelembaban']);
+    $temp = mysqli_real_escape_string($dbconnect, $data['suhu']);
+    $hum = mysqli_real_escape_string($dbconnect, $data['kelembaban']);
     $interval = date("Y-m-d h:i:s");
-    $sql = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`interval`) VALUES ('$suhu', '$kelembaban', '$interval')");
+    $sql = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`interval`) VALUES ('$temp', '$hum', '$interval')");
     if ($sql) {
         $response = "sukses";
         echo $data;
