d9c29bfe0173dc4f91eafb44c8567c4809cd8bb3
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-02-20 03:13:32 +0800

    pendeteksi banjir FULL

--- pendeteksi_banjir.php
+++ pendeteksi_banjir.php
@@ -2,18 +2,22 @@
 	include_once "connection.php";
 	
     class emp{}
+	$id_device = $_GET ["device"];
+	$banjir = $_GET ["jarak"];
     $temp = $_GET ["suhu"];
-    $hum = $_GET ["kelembaban"];
+    $hum = $_GET ["kelembaban"];	
+    $light = $_GET ["cahaya"];
+    $rain = $_GET ["hujan"];
     date_default_timezone_set('Asia/Makassar');
     $interval = date("Y-m-d h:i:s");
 	
-	if((empty($temp) || empty($hum))) {
+	if((empty($id_device) || empty($banjir) || empty($temp) || empty($hum) || empty($light) || empty($rain))) {
 		$response = new emp();
 		$response->success = 0;
 		$response->message = "Kolom isian tidak boleh kosong";
 		die(json_encode($response));
 	} else {
-		$query = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`interval`) VALUES('".$temp."','".$hum."','".$interval."')");
+		$query = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`id_device`, tb_air.`jarak_air`, tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`intensitas_cahaya`, tb_air.`hujan`, tb_air.`interval`) VALUES('".$id_device."','".$banjir."','".$temp."','".$hum."','".$light."','".$rain."','".$interval."')");
 		
 		if ($query) {
 			$response = new emp();
