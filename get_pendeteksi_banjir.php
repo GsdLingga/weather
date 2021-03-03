339ca37e3e1f1a6b75ca9802cdc0194224e87393
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-03-04 04:57:39 +0800

    get pendeteksi angin

--- get_pendeteksi_banjir.php
+++ get_pendeteksi_banjir.php
@@ -4,15 +4,21 @@
     class emp{}
 	$id_device = $_GET ["id_device"];
 	
-    $query = mysqli_query($dbconnect, "SELECT * FROM tb_air WHERE tb_air.`id_device` = $id_device ORDER BY tb_air.`id` DESC LIMIT 1;");
-    
-    $json = array();
-
-    while($row = mysqli_fetch_assoc($query)){
-        array_push($json, $row);
-    }
-    
-    echo json_encode($json);
-
+	if((empty($id_device) )) {
+		$response = new emp();
+		$response->success = 0;
+		$response->message = "Id Device tidak boleh kosong";
+		die(json_encode($response));
+	} else {
+		$query = mysqli_query($dbconnect, "SELECT * FROM tb_air WHERE tb_air.`id_device` = $id_device ORDER BY tb_air.`id` DESC LIMIT 1;");
+		
+		$json = array();
+	
+        while($row = mysqli_fetch_assoc($query)){
+            array_push($json, $row);
+        }
+        
+        echo json_encode($json);
+	}
 	mysqli_close($dbconnect);
 ?>
