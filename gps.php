0fb8a2f3ca67bb0553e9c3bc314ca29ba4e178f8
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-03-28 06:44:56 +0800

    modul_pengirim gps

--- gps.php
+++ gps.php
@@ -5,16 +5,17 @@
 	$id_device = $_GET ["device"];
 	$lat = $_GET ["lat"];
     $lng = $_GET ["lng"];
+	$sender = $_GET ["sender"];
     date_default_timezone_set('Asia/Makassar');
     $interval = date("Y-m-d h:i:s");
 	
-	if((empty($lat) || empty($lng))) {
+	if((empty($lat) || empty($lng) || empty($sender))) {
 		$response = new emp();
 		$response->success = 0;
 		$response->message = "Kolom isian tidak boleh kosong";
 		die(json_encode($response));
 	} else {
-		$query = mysqli_query($dbconnect, "INSERT INTO tb_gps (tb_gps.`id_device`, tb_gps.`latitude`, tb_gps.`longitude`, tb_gps.`interval`) VALUES('".$id_device."','".$lat."','".$lng."','".$interval."')");
+		$query = mysqli_query($dbconnect, "INSERT INTO tb_gps (tb_gps.`id_device`, tb_gps.`latitude`, tb_gps.`longitude`, tb_gps.`interval`, tb_gps.`modul_pengirim`) VALUES('".$id_device."','".$lat."','".$lng."','".$interval."','".$sender."')");
 		
 		if ($query) {
 			$response = new emp();
