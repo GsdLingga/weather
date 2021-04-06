6d93493dc644a0ef7f79eb520783896818b8db41
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2021-04-07 03:29:18 +0800

    modul_pengirim gps pendeteksi banjir

--- pendeteksi_banjir.php
+++ pendeteksi_banjir.php
@@ -8,16 +8,17 @@
     $hum = $_GET ["kelembaban"];	
     $light = $_GET ["cahaya"];
     $rain = $_GET ["hujan"];
+	$sender = $_GET ["sender"];
     date_default_timezone_set('Asia/Makassar');
     $interval = date("Y-m-d h:i:s");
 	
-	if((empty($id_device) || empty($banjir) || empty($temp) || empty($hum) || empty($light) || empty($rain))) {
+	if((empty($id_device) || empty($banjir) || empty($temp) || empty($hum) || empty($light) || empty($rain) || empty($sender))) {
 		$response = new emp();
 		$response->success = 0;
 		$response->message = "Kolom isian tidak boleh kosong";
 		die(json_encode($response));
 	} else {
-		$query = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`id_device`, tb_air.`jarak_air`, tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`intensitas_cahaya`, tb_air.`hujan`, tb_air.`interval`) VALUES('".$id_device."','".$banjir."','".$temp."','".$hum."','".$light."','".$rain."','".$interval."')");
+		$query = mysqli_query($dbconnect, "INSERT INTO tb_air (tb_air.`id_device`, tb_air.`jarak_air`, tb_air.`suhu`, tb_air.`kelembaban`, tb_air.`intensitas_cahaya`, tb_air.`hujan`, tb_air.`interval`, tb_air.`modul_pengirim`) VALUES('".$id_device."','".$banjir."','".$temp."','".$hum."','".$light."','".$rain."','".$interval."','".$sender."')");
 		
 		if ($query) {
 			$response = new emp();
